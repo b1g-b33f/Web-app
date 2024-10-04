5f1d46913de7db9bb5b3a2ff49e7a0125978d42b
Shawn Szczepkowski <130265537+b1g-b33f@users.noreply.github.com>
2024-10-04 14:59:03 -0400

    Update wasa.sh

--- wasa.sh
+++ wasa.sh
@@ -16,44 +16,22 @@ DATE=$(date +"%Y-%m-%d")
 SANITIZED_TARGET=$(echo "$TARGET" | sed 's/[^a-zA-Z0-9]/_/g')
 OUTPUT_FILE="${SANITIZED_TARGET}_${DATE}_scan_results.txt"
 
-print_header() {
-  echo "=== $1 ===" >> "$OUTPUT_FILE"
-  echo "-----------------------------" >> "$OUTPUT_FILE"
-  date >> "$OUTPUT_FILE"
-  echo "-----------------------------" >> "$OUTPUT_FILE"
-}
-
 echo "Saving results to $OUTPUT_FILE"
-
-gather_headers() {
-  echo "Gathering HTTP headers from $TARGET"
-  print_header "HTTP Headers"
-  curl -I "$1" >> "$OUTPUT_FILE" 2>&1
-}
 
 nmap_scan() {
   echo "Starting Nmap scan on $TARGET"
-  print_header "Nmap Scan"
   nmap -Pn -T4 -A -v -p- "$TARGET" >> "$OUTPUT_FILE" 2>&1
 }
 
 sslscan_scan() {
   echo "Starting sslscan on $TARGET"
-  print_header "sslscan Scan"
   sslscan "$TARGET" >> "$OUTPUT_FILE" 2>&1
 }
 
 nikto_scan() {
   echo "Starting Nikto scan on $TARGET"
-  print_header "Nikto Scan"
-  nikto -h "$TARGET" >> "$OUTPUT_FILE" 2>&1
+  nikto -h "$TARGET" -k >> "$OUTPUT_FILE" 2>&1
 }
-
-gather_headers "$1" &
-GATHER_HEADERS_PID=$!
-
-wait $GATHER_HEADERS_PID
-echo "Gathering HTTP headers completed."
 
 nmap_scan &
 NMAP_PID=$!
@@ -73,4 +51,3 @@ echo "Nikto scan completed."
 echo "All scans completed for $TARGET. Results saved to $OUTPUT_FILE."
 
 exit 0
-
